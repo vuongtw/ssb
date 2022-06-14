eb34c19a697680eaa9abe8d1126cb119c38fff2a
Vuong Pham <vuong.pham@thoughtworks.com>
2022-06-14 09:29:20 +0700

    Added greeting spring boot application

---
+++ src/main/java/io/github/vuongtw/ssb/SsbApplication.java
@@ -0,0 +1,13 @@
+package io.github.vuongtw.ssb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsbApplication.class, args);
+	}
+
+}
