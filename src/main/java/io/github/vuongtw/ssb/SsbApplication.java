a32c688a16f15218f79c4ffedf0bb2b85b8bb0b1
Vuong Pham <vuong.pham@thoughtworks.com>
2022-06-24 15:56:35 +0700

    Add database connection & ORM & migration mgnt to dev env

--- src/main/java/io/github/vuongtw/ssb/SsbApplication.java
+++ src/main/java/io/github/vuongtw/ssb/SsbApplication.java
@@ -1,13 +1,17 @@
 package io.github.vuongtw.ssb;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+@Log4j2
 @SpringBootApplication
 public class SsbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsbApplication.class, args);
 	}
-
 }
